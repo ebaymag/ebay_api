9fafb9d298019e143d1eef86d9dca85b4c1b8f55
yanmeng <974166167@qq.com>
2024-03-04 11:32:35 +0800

    process ebaymag internal api.

--- lib/ebay_api/operations/commerce.rb
+++ lib/ebay_api/operations/commerce.rb
@@ -10,6 +10,5 @@ class EbayAPI
     require_relative "commerce/notification"
     require_relative "commerce/taxonomy"
     require_relative "commerce/translation"
-    require_relative "commerce/i/ebaymagapi"
   end
 end
